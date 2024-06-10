8dfae8b3affbc6cafcd2a1ac37ede77a0e3c63e7
Steve <za19012397@zapopan.tecmm.edu.mx>
2024-06-09 21:15:59 -0600

    se cargo la libreria ui

---
+++ src/componentes/Card.jsx
@@ -0,0 +1,67 @@
+import './componentes.css';
+
+export function Card({ children }) {
+    return (
+        <div className="card-basic">{children}</div>
+    );
+}
+
+export function CardTitle({ children }) {
+    return (
+        <div className="card-title">
+            {children}
+        </div>
+    );
+}
+
+export function CardText({ children }) {
+    return (
+        <div className="card-text">
+            {children}
+        </div>
+    );
+}
+
+export function CardLink({ url, text }) {
+    return (
+        <a className="card-link" rel="noopener noreferrer" target="_blank" href={url}>{text}</a>
+    );
+}
+
+export function CardAccions({ children }) {
+    return (
+        <div className="card-actions">
+            {children}
+        </div>
+    );
+}
+
+export function CardImg({ children, urlImg }) {
+    return (
+        <div className="card" style={
+            {
+                backgroundSize: 'cover',
+                backgroundImage: `url(/Assets/${urlImg})`,
+            }
+        }>
+            {children}
+        </div>
+
+    );
+}
+
+export function CardImgContent({ children }) {
+    return (
+        <div className="card-img-content">
+            {children}
+        </div>
+    );
+}
+
+export function CardContent({children}){
+    return(
+        <div className="card-content">
+            {children}
+        </div>
+    );
+}
