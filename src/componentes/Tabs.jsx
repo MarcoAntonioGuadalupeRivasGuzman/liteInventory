b337936e724733f0595ee0d02279b13988d8e86b
Steve <za19012397@zapopan.tecmm.edu.mx>
2024-06-09 21:11:13 -0600

    se cargo la libreria ui

---
+++ src/componentes/Tabs.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import { DivCol, DivRow } from "./contenedores";
+import './componentes.css';
+
+export function Tabs({ headers, elements }) {
+
+    const [currentTab, setCurrentTab] = useState(1);
+
+    const onTabChange = (id) => {
+        //console.log('id',id)
+        setCurrentTab(id)
+        //console.log('tab', currentTab)
+    }
+
+    return (
+        <DivCol>
+            <DivRow>
+                {
+                    headers.map(head =>
+                        <div key={head.title} className="header-tabs">
+                            <p onClick={() => onTabChange(head.id)} className={currentTab === head.id ? 'tab tab-actibe' : 'tab'}>{head.title}</p>
+                        </div>
+                    )
+                }
+            </DivRow>
+            <DivRow>
+                {
+                    headers.map(head =>
+                        <div key={head.id} className={currentTab === head.id ? '' : 'tab-close'}>
+                            {head.content}
+                        </div>
+                    )
+                }
+            </DivRow>
+        </DivCol>
+    )
+}
