afca67b4071eb8abe654a42a2f50b2bf82ec17c3
Steve <za19012397@zapopan.tecmm.edu.mx>
2024-06-16 09:23:41 -0600

    se agrego la pagina del home

---
+++ src/pages/home.jsx
@@ -0,0 +1,12 @@
+import { MainFrame } from "../componentes/contenedores";
+
+export function Home() {
+    return (
+        <MainFrame>
+            <div>
+                <h1>Home page</h1>
+            </div>
+        </MainFrame>
+
+    )
+}
