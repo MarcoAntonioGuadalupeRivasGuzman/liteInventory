275a78ed9f3b2c74366e42d57ac6d0b8565426e0
Steve <za19012397@zapopan.tecmm.edu.mx>
2024-06-16 09:21:10 -0600

    se agregaron algnunos elementos para mejorar el aspecto del login

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,24 +1,28 @@
 import { Boton } from "../componentes/Boton";
-import { DivCanva, DivCol, DivRow, Panel } from "../componentes/contenedores";
+import { DivCanva, DivCol, DivFull, DivRow, MainFrame, Panel } from "../componentes/contenedores";
 import { TextField } from "../componentes/TextField";
 
 export function Login() {
     return (
         <>
-            <Panel>
-                <DivRow>
-                    <img className="img-login" src="/Assets/montain.jpg" />
-                    <DivCanva>
-                        <DivCol>
-                            <h1 className="alter-subtitle">Bienvenido a lite inventory</h1>
-                            <p className="main-content">Inicia sesion</p>
-                            <TextField label={"Usuario"} />
-                            <TextField label={"Password"} isPass={true} />
-                            <Boton color={'principal'}>Login</Boton>
-                        </DivCol>
-                    </DivCanva>
-                </DivRow>
-            </Panel>
+            <MainFrame>
+                <DivFull>
+                    <Panel>
+                        <DivRow>
+                            <img className="img-login" src="/Assets/montain.jpg" />
+                            <DivCanva>
+                                <DivCol>
+                                    <h1 className="alter-subtitle">Bienvenido a lite inventory</h1>
+                                    <p className="main-content">Inicia sesion</p>
+                                    <TextField label={"Usuario"} />
+                                    <TextField label={"Password"} isPass={true} />
+                                    <Boton color={'principal'}>Login</Boton>
+                                </DivCol>
+                            </DivCanva>
+                        </DivRow>
+                    </Panel>
+                </DivFull>
+            </MainFrame>
         </>
     )
 }
